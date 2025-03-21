9c1d6edab4ea2b5df5c4b96d76dd02c64873e4f3
Andrew <ad13.works@gmail.com>
2025-03-21 04:12:50 +0100

    Test

--- src/pages/TicTacToePage.tsx
+++ src/pages/TicTacToePage.tsx
@@ -4,7 +4,7 @@ import TicTacToe from '@features/tictactoe/ui/TicTacToe';
 
 const TicTacToePage: FC = () => (
   <div className="container">
-    <h1>Tic-Tac-Toe</h1>
+    <h1>Tic-Tac-Toe (Test)</h1>
     <TicTacToe />
   </div>
 );
